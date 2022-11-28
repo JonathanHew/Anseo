0359f002bb25391cadf2305d58b4f266c887dbdb
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-11-28 13:03:37 +0000

    login validation to check for errors implemented

--- server/src/validators/auth.js
+++ server/src/validators/auth.js
@@ -1,5 +1,6 @@
 const { check } = require("express-validator");
 const db = require("../db");
+const { compare } = require("bcryptjs");
 
 // password valid check using express-validator
 const password = check("password")
@@ -7,7 +8,7 @@ const password = check("password")
   .withMessage("Password must be between 6 and 15 characters!");
 
 // email valid check
-const email = check("email").isEmail().withMessage("Invalid Email Address!");
+const email = check("email").isEmail().withMessage("Invalid email address!");
 
 // email already existss check
 const emailExists = check("email").custom(async (value) => {
@@ -16,10 +17,31 @@ const emailExists = check("email").custom(async (value) => {
   ]);
 
   if (rows.length) {
-    throw new Error("Email Already Exists!");
+    throw new Error("Email already exists!");
+  }
+});
+
+// login validation
+const loginFieldsCheck = check("email").custom(async (value, { req }) => {
+  const user = await db.query("SELECT * FROM users WHERE user_email = $1", [
+    value,
+  ]);
+
+  if (!user.rows.length) {
+    throw new Error("This email does not exist!");
+  }
+
+  // check password if email exists
+  const validPassword = await compare(req.body.password, user.rows[0].user_password);
+
+
+  if(!validPassword)
+  {
+    throw new Error("Password is incorrect!");
   }
 });
 
 module.exports = {
   registerValidation: [email, password, emailExists],
+  loginValidation: [loginFieldsCheck],
 };
