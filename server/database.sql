fc13eec47f349ee12810a7de625749d09127990b
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-11-29 15:25:23 +0000

    db file updated, signIn and lists table created

--- server/database.sql
+++ server/database.sql
@@ -5,3 +5,21 @@ CREATE TABLE users(
     user_password VARCHAR(255) NOT NULL,
     created_at DATE DEFAULT current_date
 );
+
+--lists table
+CREATE TABLE lists(
+    list_id SERIAL PRIMARY KEY
+    list_name
+);
+
+--signIns table
+CREATE TABLE signIns(
+    signIn_id SERIAL PRIMARY KEY,
+    signIn_name VARCHAR(255) NOT NULL,
+    signIn_number VARCHAR(255) NOT NULL,
+    list_id INTEGER,
+    CONSTRAINT fk_list
+        FOREIGN KEY(list_id)
+            REFERENCES lists(list_id)
+                ON DELETE CASCADE
+);
