fe4cac8688e9a711ffb196fe0e899e678defbd0f
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-11-30 11:55:09 +0000

    fixed QR code URL to the correct one

--- client/src/pages/display.js
+++ client/src/pages/display.js
@@ -3,14 +3,14 @@ import QRCodeSVG from "qrcode.react";
 
 const Display = () => {
 
-    //const url = "http://localhost:8000/api/sign-in";
+    //const url = "http://localhost:3000/sign-in";
 
     return (
       <Layout>
         <h1>Sign In By Scanning the QR Code!</h1>
         <div class="row mt-5">
             <div class="col-8">
-                <QRCodeSVG value = "http://localhost:8000/api/sign-in" size={256}/>
+                <QRCodeSVG value = "http://localhost:3000/sign-in" size={256}/>
             </div>
             <div class="col-4">
               <p>Student Count:</p>
