69e81f0c86dfb03e941323ec65ad1d43366cd215
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-01-23 13:23:26 +0000

    added a relationship between user and lists in the database

--- server/database.sql
+++ server/database.sql
@@ -1,3 +1,10 @@
+CREATE DATABASE anseo_v2;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS signIns;
+DROP TABLE IF EXISTS lists;
+DROP TABLE IF EXISTS users;
+
 -- users table 
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY, 
@@ -8,8 +15,13 @@ CREATE TABLE users(
 
 --lists table
 CREATE TABLE lists(
-    list_id SERIAL PRIMARY KEY,
-    list_name VARCHAR(255) NOT NULL
+    list_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    list_name VARCHAR(255) NOT NULL, 
+    user_id INTEGER NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES users(user_id)
+                ON DELETE CASCADE
 );
 
 --signIns table
@@ -17,7 +29,7 @@ CREATE TABLE signIns(
     signIn_id SERIAL PRIMARY KEY,
     signIn_name VARCHAR(255) NOT NULL,
     signIn_number VARCHAR(255) NOT NULL,
-    list_id INTEGER,
+    list_id uuid,
     CONSTRAINT fk_list
         FOREIGN KEY(list_id)
             REFERENCES lists(list_id)
@@ -26,6 +38,6 @@ CREATE TABLE signIns(
 
 
 
-
-INSERT INTO lists (list_id, list_name) VALUES (1, "list1");
-INSERT INTO signIns (signIn_id, signIn_name, signIn_number, list_id) VALUES (1, 'Jonathan', 'C19472842', 1);
+INSERT INTO users (user_email, user_password) VALUES ('jonathan@gmail.com', 'password');
+INSERT INTO lists (list_name, user_id) VALUES ('jonathan-list1', 1);
+INSERT INTO signIns (signIn_id, signIn_name, signIn_number, list_id) VALUES (1, 'Jonathan', 'C19472842', '7dfc25b1-fe78-48dc-ad7a-1bacb2c8ba60');
