ea22897afe0e47e4f3239da221bf83d57c83dd73
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-01-26 17:17:43 +0000

    table structure from bootstrap added

--- client/src/pages/sessions.js
+++ client/src/pages/sessions.js
@@ -1,19 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchUserSessions } from "../api/lecturer.api";
 import Layout from "../components/layout";
 
 const Sessions = ({ id }) => {
-  
+  const [sessions, setSessions] = useState([]);
+
   useEffect(() => {
     (async () => {
-      const content = await fetchUserSessions({id});
-      console.log(content);
+      const content = await fetchUserSessions({ id });
+      setSessions(content.data.sessions);
     })();
   });
+
   return (
     <Layout>
       <h1>Sessions Page</h1>
       <p>User ID: {id}</p>
+
+      <table class="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Date</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            <tr>
+              <td>Jonathans List</td>
+              <td>26/01/2023</td>
+              <td>17:00</td>
+            </tr>
+          }
+          
+        </tbody>
+      </table>
     </Layout>
   );
 };
