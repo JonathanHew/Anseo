d6475024408c762121e6020f7540d529b96bcbf9
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-01-31 17:11:26 +0000

    created controller function to return students given a session_id

--- server/src/controllers/lecturer.controller.js
+++ server/src/controllers/lecturer.controller.js
@@ -1,7 +1,6 @@
 const db = require("../db");
 
 exports.createSession = async (req, res) => {
-  console.log(req.body)
   const { user_id, session_name } = req.body;
   try {
     await db.query(
@@ -24,10 +23,31 @@ exports.createSession = async (req, res) => {
 exports.getSessions = async (req, res) => {
   const user_id = req.user.id;
   try {
-    const { rows } = await db.query(`SELECT * FROM sessions WHERE user_id = $1`, [user_id]);
+    const { rows } = await db.query(
+      `SELECT * FROM sessions WHERE user_id = $1`,
+      [user_id]
+    );
     return res.status(200).json({
       success: true,
       sessions: rows,
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+exports.getStudentsBySessionId = async (req, res) => {
+  const { session_id } = req.body;
+  try {
+    const { rows } = await db.query(
+      `SELECT * FROM SignIns WHERE session_id = $1`,
+      [session_id]
+    );
+
+    return res.status(200).json({
+      success: true,
+      students: rows,
+      count: rows.length,
     });
   } catch (err) {
     console.error(err.message);
