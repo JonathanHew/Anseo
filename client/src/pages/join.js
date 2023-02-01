7e8d7e9c446dd95024ebcc7a8d4046fb91b08df5
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-01 12:56:12 +0000

    copied useEffect from sessions page into join page

--- client/src/pages/join.js
+++ client/src/pages/join.js
@@ -1,10 +1,21 @@
 import Layout from "../components/layout";
 import QRCode from "react-qr-code";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const Join = () => {
   const { id } = useParams();
   const url = "http://localhost:3000/sign-in/" + id;
+  const [active, setActive] = useState(false);
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      //const content = await fetchUserSessions({ id });
+      //setSessions(content.data.sessions);
+      //setLoading(false);
+    })();
+  }, []);
 
   return (
     <Layout>
