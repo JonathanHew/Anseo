98308bc8d2a24091d51391c36faf4701358bae2d
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-01 12:58:59 +0000

    export api function created on frontend to requests students in session from server

--- client/src/api/lecturer.api.js
+++ client/src/api/lecturer.api.js
@@ -2,15 +2,18 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export async function fetchUserSessions(user_id) {
-    return await axios.get(
-      "http://localhost:8000/api/get-sessions",
-      {user_id: user_id}
-    );
-  }
+  return await axios.get("http://localhost:8000/api/get-sessions", {
+    user_id: user_id,
+  });
+}
 
 export async function onCreateSession(session_data) {
   return await axios.post(
     "http://localhost:8000/api/create-session",
-    session_data 
+    session_data
   );
+}
+
+export async function fetchStudentsInSession(session_id) {
+  return await axios.get("/get-students-in-session", session_id);
 }
