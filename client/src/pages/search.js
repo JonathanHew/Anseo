9699c359e83195d5d429eeb9ed17861d0fe3bc4b
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-05 17:31:13 +0000

    form added to search page

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React, { useState } from "react";
+import Layout from "../components/layout";
 
 const Search = () => {
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    console.log("Form Submitted!!!");
+  };
+
   return (
     <Layout>
-        
+      <h1 className="text-center mt-5">Search Student</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          //value={description}
+          //onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Search</button>
+      </form>
     </Layout>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
