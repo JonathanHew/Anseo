e28522c2b591cf43be60aa9d192a1063bac36970
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-05 16:40:31 +0000

    controller function created to get session for the searched student

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <Layout>
+        
+    </Layout>
   )
 }
 
--- server/src/controllers/lecturer.controller.js
+++ server/src/controllers/lecturer.controller.js
@@ -56,7 +56,7 @@ exports.getStudentsBySessionId = async (req, res) => {
 exports.setSessionStatus = async (req, res) => {
   const { session_id } = req.body;
   try {
-    const {rows} = await db.query(
+    const { rows } = await db.query(
       `UPDATE sessions SET session_is_active = NOT session_is_active WHERE session_id = $1 returning session_is_active`,
       [session_id]
     );
@@ -74,7 +74,7 @@ exports.setSessionStatus = async (req, res) => {
 exports.getSessionStatus = async (req, res) => {
   const { session_id } = req.body;
   try {
-    const {rows} = await db.query(
+    const { rows } = await db.query(
       `SELECT session_is_active FROM sessions WHERE session_id = $1`,
       [session_id]
     );
@@ -86,5 +86,22 @@ exports.getSessionStatus = async (req, res) => {
   } catch (err) {
     console.error(err.message);
   }
-}; 
+};
 
+exports.getSessionsForStudent = async (req, res) => {
+  const user_id = req.user.id;
+  const { student_number } = req.body;
+
+  try {
+    const { rows } = await db.query(
+      `SELECT sessions.session_id, session_name, session_date, session_time FROM sessions JOIN signIns ON sessions.session_id = signIns.session_id WHERE signin_number = $1 and user_id = $2`,
+      [student_number, user_id]
+    );
+    return res.status(200).json({
+      success: true,
+      sessions: rows,
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+};
