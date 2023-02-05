d99acc6874b994147bb17acaa786f92f7f08b1bb
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-05 16:56:38 +0000

    validation to check if student number is 9 digits and exists implemented

--- server/src/validators/lecturer.validator.js
+++ server/src/validators/lecturer.validator.js
@@ -6,10 +6,24 @@ const sessionName = check("session_name")
   .notEmpty()
   .withMessage("Session name must not be empty");
 
-const userId = check("user_id")
-  .notEmpty()
-  .withMessage("User ID not found!");
+const userId = check("user_id").notEmpty().withMessage("User ID not found!");
+
+const studentNumber = check("number")
+  .isLength(9)
+  .withMessage("Student number should be 9 charachters long!");
+
+const studentExists = check("student_number").custom(async (value, { req }) => {
+  const { rows } = await db.query(
+    "SELECT * FROM signIns WHERE signIn_number = $1",
+    [req.body.student_number]
+  );
+
+  if (!rows.length) {
+    throw new Error("Student does not exist in any sessions!");
+  }
+});
 
 module.exports = {
-  sessionValidator: [ sessionName, userId],
+  sessionValidator: [sessionName, userId],
+  studentSearchValidator: [studentNumber, studentExists],
 };
