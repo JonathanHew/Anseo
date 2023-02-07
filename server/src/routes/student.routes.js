aab66b3f6a2011628ca2001a571b7aa546c512ee
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-07 17:04:01 +0000

    created route for getSessionByPin, uses pinValidator and validationMiddleware

--- server/src/routes/student.routes.js
+++ server/src/routes/student.routes.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
-const { studentSignIn } = require("../controllers/student.controller");
+const { studentSignIn, getSessionByPin } = require("../controllers/student.controller");
 const { validationMiddleware } = require("../middlewares/validations-middleware");
 const { studentValidator, pinValidator } = require("../validators/student.validator");
 const router = Router();
 
 router.post("/sign-in", studentValidator, validationMiddleware, studentSignIn);
-router.post("/pin"), 
+router.post("/pin", pinValidator, validationMiddleware, getSessionByPin);
 
 module.exports = router;
