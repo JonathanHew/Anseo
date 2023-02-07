b02edda5388253b7cb11f856a4e574a14dfe9415
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-07 17:02:57 +0000

    Created a validator function to check PIN is length 6 and is alphanumeric

--- server/src/routes/student.routes.js
+++ server/src/routes/student.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const { studentSignIn } = require("../controllers/student.controller");
 const { validationMiddleware } = require("../middlewares/validations-middleware");
-const { studentValidator } = require("../validators/student.validator");
+const { studentValidator, pinValidator } = require("../validators/student.validator");
 const router = Router();
 
 router.post("/sign-in", studentValidator, validationMiddleware, studentSignIn);
+router.post("/pin"), 
 
 module.exports = router;
--- server/src/validators/student.validator.js
+++ server/src/validators/student.validator.js
@@ -16,7 +16,7 @@ const studentExists = check("number").custom(async (value, { req }) => {
 // check student number is 9 digits long
 const studentNumber = check("number")
   .isLength(9)
-  .withMessage("Student number should be 9 character long!");
+  .withMessage("Student number should be 9 characters long!");
 
 // check name is not empty and is alphabetic
 const studentName = check("name")
@@ -25,6 +25,13 @@ const studentName = check("name")
   .matches(/^[A-Za-z\s]+$/)
   .withMessage("Name must be alphabetic.");
 
+const pinNumber = check("session_pin")
+.isLength(6)
+.withMessage("PIN Code Should be 6 characters long!")
+.isAlphanumeric()
+.withMessage("PIN Code should only contain letters and numbers!")
+
 module.exports = {
   studentValidator: [studentNumber, studentExists, studentName],
+  pinValidator: [pinNumber],
 };
