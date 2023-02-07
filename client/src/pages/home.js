26dae873c107e2402efb3df595a880fb9a0241ef
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-07 18:48:57 +0000

    added form with 1 input for session pin on home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,13 +1,28 @@
 import Layout from "../components/layout";
 
 const Home = () => {
-    return (
-      <Layout>
-        <h1>Home</h1>
-        <p>Welcome to Anseo!</p>
-      </Layout>
-    );
-  };
-  
-  export default Home;
-  
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Welcome to Anseo!</p>
+      <div
+        className="container text-center"
+        style={{ width: "35%" }}
+      >
+        <div class="form-floating mt-5">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingInput"
+            placeholder="Session PIN"
+            required
+          />
+          <label for="floatingInput">Session PIN</label>
+          <button type="button" class="btn btn-secondary btn-lg mt-1 w-100">Join</button>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
