98fe6fe316fdf7e05e9d61fe01789cd366f007ab
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-08 12:07:30 +0000

    onPinSubmit API function created

--- client/src/api/signIn.api.js
+++ client/src/api/signIn.api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export async function onSignIn(signInData) {
-    return await axios.post(
-      "http://localhost:8000/api/sign-in",
-      signInData
-    );
-  }
+  return await axios.post("http://localhost:8000/api/sign-in", signInData);
+}
+
+export async function onPinSubmit(pin) {
+  return await axios.post("http://localhost:8000/api/pin", {
+    session_pin: pin
+  });
+}
