70ae4f7b72368c01a90fb76dec22ffbe09143516
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-08 12:20:53 +0000

    When pin form is submitted, it passes pin to server and server responds with corresponding session

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,23 +1,23 @@
 import { useState } from "react";
+import { onPinSubmit } from "../api/signIn.api";
 import Layout from "../components/layout";
 
 const Home = () => {
-  const [pin, setPin] = useState();
-  const [error, setError] = useState("Error!");
+  const [pin, setPin] = useState("");
+  const [error, setError] = useState("");
 
   const onSubmitForm = async (e) => {
     e.preventDefault();
     setError("");
     try {
-      //const content = await fetchSessionsForStudent(number);
-      //setSessions(content.data.sessions);
-      //setNumber("");
-      //setMessage("");
+      const content = await onPinSubmit(pin);
+      const session_id = content.data.result[0].session_id;
+      console.log(session_id);
+
+      setPin("");
     } catch (err) {
-      //console.error(err.response.data.errors[0].msg);
-      //setError(err.response.data.errors[0].msg);
-      //setSessions([]);
-      //setMessage("No sessions available")
+      console.error(err.response.data.errors[0].msg);
+      setError(err.response.data.errors[0].msg);
     }
   };
 
@@ -26,22 +26,24 @@ const Home = () => {
       <h1>Home</h1>
       <p>Welcome to Anseo!</p>
       <div className="container text-center" style={{ width: "35%" }}>
-        <div class="form-floating mt-5">
-          <input
-            type="text"
-            class="form-control"
-            id="floatingInput"
-            placeholder="Session PIN"
-            value={pin}
-            onChange={(e) => setPin(e.target.value)}
-            required
-          />
-          <label for="floatingInput">Session PIN</label>
-          <div style={{ color: "red", margin: "10px 0" }}>{error}</div>
-          <button type="button" class="btn btn-secondary btn-lg mt-1 w-100">
-            Join
-          </button>
-        </div>
+        <form onSubmit={onSubmitForm}>
+          <div class="form-floating mt-5">
+            <input
+              type="text"
+              class="form-control"
+              id="floatingInput"
+              placeholder="Session PIN"
+              value={pin}
+              onChange={(e) => setPin(e.target.value)}
+              required
+            />
+            <label for="floatingInput">Session PIN</label>
+            <div style={{ color: "red", margin: "10px 0" }}>{error}</div>
+            <button class="btn btn-secondary btn-lg mt-1 w-100">
+              Join
+            </button>
+          </div>
+        </form>
       </div>
     </Layout>
   );
