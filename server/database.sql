f97a4d4653425f70a9a9c4b4ca62751bc6f35681
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-11 12:10:55 +0000

    reordered code in database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,56 +1,13 @@
 CREATE DATABASE anseo_v2;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DROP TABLE IF EXISTS signIns;
-DROP TABLE IF EXISTS sessions;
-DROP TABLE IF EXISTS users;
-
 DROP FUNCTION IF EXISTS GEN_RANDOM_BYTES;
 DROP FUNCTION IF EXISTS RANDOM_STRING;
 DROP FUNCTION IF EXISTS UNIQUE_RANDOM;
 
--- users table 
-CREATE TABLE users(
-    user_id SERIAL PRIMARY KEY, 
-    user_email VARCHAR(255) UNIQUE NOT NULL,
-    user_password VARCHAR(255) NOT NULL,
-    created_at DATE DEFAULT current_date
-);
-
---sessions table
-CREATE TABLE sessions(
-    session_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-    session_name VARCHAR(255) NOT NULL, 
-    session_date DATE DEFAULT current_date,
-    session_time TIME DEFAULT localtime(0),
-    session_is_active BOOLEAN DEFAULT false,
-    user_id INTEGER NOT NULL,
-    session_pin VARCHAR(6) DEFAULT UNIQUE_RANDOM(6, 'sessions', 'session_pin'),
-    CONSTRAINT fk_user
-        FOREIGN KEY(user_id)
-            REFERENCES users(user_id)
-                ON DELETE CASCADE
-);
-
---signIns table
-CREATE TABLE signIns(
-    signIn_id SERIAL PRIMARY KEY,
-    signIn_name VARCHAR(255) NOT NULL,
-    signIn_number VARCHAR(255) NOT NULL,
-    signIn_date DATE DEFAULT current_date,
-    signIn_time TIME DEFAULT localtime(0),
-    session_id uuid,
-    CONSTRAINT fk_session
-        FOREIGN KEY(session_id)
-            REFERENCES sessions(session_id)
-                ON DELETE CASCADE
-);
-
-
-INSERT INTO users (user_email, user_password) VALUES ('jonathan@gmail.com', 'password');
-INSERT INTO sessions (session_name, session_is_active, user_id) VALUES ('Dev Test2', 'f', 1);
-INSERT INTO signIns (signIn_id, signIn_name, signIn_number, session_id) VALUES (1, 'Jonathan', 'C19472842', '7dfc25b1-fe78-48dc-ad7a-1bacb2c8ba60');
-
+DROP TABLE IF EXISTS signIns;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS users;
 
 create function GEN_RANDOM_BYTES(int) returns bytea as
 '$libdir/pgcrypto', 'pg_random_bytes' language c strict;
@@ -89,3 +46,45 @@ begin
   end loop;
 end;
 $$ language plpgsql;
+
+-- users table 
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY, 
+    user_email VARCHAR(255) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    created_at DATE DEFAULT current_date
+);
+
+--sessions table
+CREATE TABLE sessions(
+    session_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    session_name VARCHAR(255) NOT NULL, 
+    session_date DATE DEFAULT current_date,
+    session_time TIME DEFAULT localtime(0),
+    session_is_active BOOLEAN DEFAULT false,
+    user_id INTEGER NOT NULL,
+    session_pin VARCHAR(6) DEFAULT UNIQUE_RANDOM(6, 'sessions', 'session_pin'),
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES users(user_id)
+                ON DELETE CASCADE
+);
+
+--signIns table
+CREATE TABLE signIns(
+    signIn_id SERIAL PRIMARY KEY,
+    signIn_name VARCHAR(255) NOT NULL,
+    signIn_number VARCHAR(255) NOT NULL,
+    signIn_date DATE DEFAULT current_date,
+    signIn_time TIME DEFAULT localtime(0),
+    session_id uuid,
+    CONSTRAINT fk_session
+        FOREIGN KEY(session_id)
+            REFERENCES sessions(session_id)
+                ON DELETE CASCADE
+);
+
+INSERT INTO users (user_email, user_password) VALUES ('jonathan@gmail.com', 'password');
+INSERT INTO sessions (session_name, session_is_active, user_id) VALUES ('Dev Test2', 'f', 1);
+INSERT INTO signIns (signIn_id, signIn_name, signIn_number, session_id) VALUES (1, 'Jonathan', 'C19472842', '7dfc25b1-fe78-48dc-ad7a-1bacb2c8ba60');
+
