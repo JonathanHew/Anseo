ae6edc1b6cbadcfae122e99e914a7728a6472d0f
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-15 14:02:39 +0000

    Added PostGIS extension to the Postgres DB, Created a new table which will hold the geometry values for campuses. Created an insert for the central quad and library locations. Created a query which uses PostGIS ST_Intersects to check if a point is inside of a location

--- server/database.sql
+++ server/database.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE anseo_v2;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "postgis";
+
 
 DROP FUNCTION IF EXISTS GEN_RANDOM_BYTES;
 DROP FUNCTION IF EXISTS RANDOM_STRING;
@@ -8,6 +10,7 @@ DROP FUNCTION IF EXISTS UNIQUE_RANDOM;
 DROP TABLE IF EXISTS signIns;
 DROP TABLE IF EXISTS sessions;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS locations;
 
 create function GEN_RANDOM_BYTES(int) returns bytea as
 '$libdir/pgcrypto', 'pg_random_bytes' language c strict;
@@ -84,7 +87,67 @@ CREATE TABLE signIns(
                 ON DELETE CASCADE
 );
 
+--table to store coordinates for campuses
+CREATE TABLE locations (
+    location_id SERIAL PRIMARY KEY,
+    location_name VARCHAR(255) NOT NULL,
+    location_polygon GEOMETRY NOT NULL
+);
+
 INSERT INTO users (user_email, user_password) VALUES ('jonathan@gmail.com', 'password');
 INSERT INTO sessions (session_name, session_is_active, user_id) VALUES ('Dev Test2', 'f', 1);
 INSERT INTO signIns (signIn_id, signIn_name, signIn_number, session_id) VALUES (1, 'Jonathan', 'C19472842', '7dfc25b1-fe78-48dc-ad7a-1bacb2c8ba60');
+
+INSERT INTO locations (location_name, location_polygon) VALUES (
+  'library',
+  ST_SetSRID(
+    'POLYGON((
+      -6.2879568 53.3578114,
+      -6.2878656 53.3576481,
+      -6.2874391 53.3577362,
+      -6.2874016 53.3576866,
+      -6.2872782 53.3577106,
+      -6.2873077 53.3577554,
+      -6.2868759 53.3578498,
+      -6.2869671 53.3580083,
+      -6.2872675 53.3579571,
+      -6.2874150 53.3582004,
+      -6.2876349 53.3581524,
+      -6.2875008 53.3579107,
+      -6.2879568 53.3578114
+    ))' :: GEOMETRY, 4326
+  )
+);
+
+INSERT INTO locations (location_name, location_polygon) VALUES (
+  'library',
+  ST_SetSRID(
+    'POLYGON((
+      -6.2824093 53.3567958,
+      -6.2823074 53.3560258,
+      -6.2821116 53.3560018,
+      -6.2819935 53.3563492,
+      -6.2812506 53.3562724,
+      -6.2812533 53.3562403,
+      -6.2813230 53.3562451,
+      -6.2814196 53.3559282,
+      -6.2818112 53.3559586,
+      -6.2818326 53.3558289,
+      -6.2811434 53.3556895,
+      -6.2809986 53.3562098,
+      -6.2811381 53.3562290,
+      -6.2811327 53.3562626,
+      -6.2809879 53.3562514,
+      -6.2809581 53.3563794,
+      -6.2811297 53.3564018,
+      -6.2810600 53.3566452,
+      -6.2824093 53.3567958
+    ))' :: GEOMETRY, 4326
+  )
+);
+
+
+select ST_Intersects(
+  (select location_polygon from locations where location_id = 2),
+  ST_SetSRID (ST_Point(-6.2817008, 53.356427):: GEOMETRY, 4326));
 
