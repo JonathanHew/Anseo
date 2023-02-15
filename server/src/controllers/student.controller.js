2ea0633099aba147d2f42ee77381074da050fff6
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-15 16:14:01 +0000

    removed checkLocation controller function as i have combined it with existing studentSignIn function. This function now also checks student location

--- server/src/controllers/student.controller.js
+++ server/src/controllers/student.controller.js
@@ -2,20 +2,23 @@ const db = require("../db");
 
 exports.studentSignIn = async (req, res) => {
   const { name, number, session_id, longitude, latitude } = req.body;
+  let locationCheck = false;
 
   try {
-    const { rows } = await db.query(
-      `SELECT ST_Intersects(
-        (SELECT location_polygon FROM locations WHERE location_id = 1),
-        ST_SetSRID (ST_Point($1, $2):: GEOMETRY, 4326))`,
-      [longitude, latitude]
-    );
+    if (longitude !== undefined) {
+      const { rows } = await db.query(
+        `SELECT ST_Intersects(
+          (SELECT location_polygon FROM locations WHERE location_id = 1),
+          ST_SetSRID (ST_Point($1, $2):: GEOMETRY, 4326))`,
+        [longitude, latitude]
+      );
 
-    console.log(rows);
+      locationCheck = rows[0].st_intersects;
+    }
 
     await db.query(
-      "INSERT INTO signIns (signIn_name, signIn_number, session_id) VALUES (INITCAP($1) , UPPER($2), $3)",
-      [name, number, session_id]
+      "INSERT INTO signIns (signIn_name, signIn_number, session_id, signIn_on_campus) VALUES (INITCAP($1) , UPPER($2), $3, $4)",
+      [name, number, session_id, locationCheck]
     );
 
     return res.status(201).json({
@@ -46,22 +49,3 @@ exports.getSessionByPin = async (req, res) => {
     console.error(err.message);
   }
 };
-
-exports.checkLocation = async (req,res) => {
-  const {longitude, latitude} = req.body;
-  try {
-    const { rows } = await db.query(
-      `SELECT ST_Intersects(
-        (SELECT location_polygon FROM locations WHERE location_id = 1),
-        ST_SetSRID (ST_Point($1, $2):: GEOMETRY, 4326))`,
-      [longitude, latitude]
-    );
-
-    return res.status(200).json({
-      success: true,
-      result: rows,
-    });
-  } catch (err) {
-    console.error(err.message);
-  }
-}
