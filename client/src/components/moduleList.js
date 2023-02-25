89979cbcebb27b379a4271cc00b325cbffa3764d
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-25 11:28:18 +0000

    Module list contains navLink which redirects to the student report page and passes the student number and module id as params

--- client/src/components/moduleList.js
+++ client/src/components/moduleList.js
@@ -1,15 +1,14 @@
 import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
-import Dashboard from "../pages/dashboard";
 
 const ModuleList = ({ modules, studentNumber }) => {
   return (
     <Fragment>
       <h4 className="text-center mt-5">Modules</h4>
-      <p>{studentNumber}</p>
+      
       <div class="list-group">
         {modules.map((module) => (
-          <NavLink to={`/join/1`}>
+          <NavLink to={`/student-report/${studentNumber}/${module.module_id}`}>
             <button
               key={module.module_id}
               type="button"
--- client/src/pages/studentReport.js
+++ client/src/pages/studentReport.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import Layout from '../components/layout';
 
 const StudentReport = () => {
+    const {student_number, module_id} = useParams();
   return (
-    <div>studentReport</div>
+    <Layout>
+        <p>student number : {student_number}</p>
+        <p>module id : {module_id}</p>
+    </Layout>
   )
 }
 
