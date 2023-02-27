27b1c6547a22634c555d397ed0a085966ac079dd
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-27 12:21:08 +0000

    created a state which will contain all pie chart details. Running into an issue where the pie chart is rendering before the pie data is worked out therefore resulting in 0 values

--- client/src/pages/studentReport.js
+++ client/src/pages/studentReport.js
@@ -5,12 +5,14 @@ import {
   fetchSignInsForStudentInModule,
 } from "../api/lecturer.api";
 import Layout from "../components/layout";
+import PieChart from "../components/pieChart";
 
 const StudentReport = () => {
   const { student_number, module_id } = useParams();
   const [signins, setSignins] = useState([]);
   const [sessions, setSessions] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [piedata, setPiedata] = useState({});
 
   useEffect(() => {
     (async () => {
@@ -19,10 +21,23 @@ const StudentReport = () => {
         module_id
       );
 
-      
       const sessionContent = await fetchSessionsForModule(module_id);
       setSignins(signinContent.data.signins);
       setSessions(sessionContent.data.sessions);
+
+      setPiedata({ 
+        labels: ["Attended", "Not Attended"],
+        datasets: [
+          {
+            label: "Number",
+            data: [signins.length, sessions.length - signins.length],
+            backgroundColor: ["green", "red"],
+            borderColor: "black",
+            borderWidth: 1,
+          },
+        ],
+      });
+      console.log(piedata);
       setLoading(false);
     })();
   }, []);
@@ -32,6 +47,7 @@ const StudentReport = () => {
     </Layout>
   ) : (
     <Layout>
+      <PieChart chartData={piedata}></PieChart>
       <h4 className="text-center mt-5">Sign Ins</h4>
       <table className="table text-center">
         <thead>
