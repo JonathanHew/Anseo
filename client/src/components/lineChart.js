9d2e46d12117e16bf2fed0f0923ab81a730bd8fc
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-02-28 17:17:41 +0000

    Created linechart component which will display linecharts given data and options

---
+++ client/src/components/lineChart.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+
+const LineChart = ({ chartData, options }) => {
+  return (
+    <div style={{"width": "500px" }}>
+      <Line data={chartData} options={options}/>
+    </div>
+  );
+};
+
+export default LineChart;
