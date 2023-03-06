e663ca4836ae3b4f039e2250299c41894cc7bd9c
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-06 15:11:22 +0000

    added loading screen onto modules page

--- client/src/pages/modules.js
+++ client/src/pages/modules.js
@@ -3,17 +3,23 @@ import { fetchUserModules } from "../api/lecturer.api";
 import Layout from "../components/layout";
 
 const Modules = () => {
-
   const [modules, setModules] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       const moduleContent = await fetchUserModules();
       setModules(moduleContent.data.modules);
-      //setLoading(false);
+      setLoading(false);
     })();
   }, []);
-  return <Layout>Modules Page</Layout>;
+  return loading ? (
+    <Layout>
+      <h1>Loading...</h1>
+    </Layout>
+  ) : (
+    <Layout>Modules Page</Layout>
+  );
 };
 
 export default Modules;
