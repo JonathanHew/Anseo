8fd63c83b7284e814dbfbac6151f2d5bf2ba8f12
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-06 17:48:05 +0000

    implemented useEffect hook to fetch module line data from server and store result in useState

--- client/src/pages/moduleReport.js
+++ client/src/pages/moduleReport.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchModuleReportLineData } from "../api/lecturer.api";
 import Layout from "../components/layout";
 
 const ModuleReport = () => {
-  
-  return <Layout>
-    Module Report Page 
-  </Layout>;
+  const { module_id } = useParams();
+  const [sessions, setSessions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const sessionContent = await fetchModuleReportLineData(module_id);
+      setSessions(sessionContent.data.sessions);
+      setLoading(false);
+    })();
+  }, []);
+
+  return <Layout>Module Report Page</Layout>;
 };
 
 export default ModuleReport;
