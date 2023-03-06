f6cbf66cd6cbec08bad30f4cb6ba72cbf65f2fb7
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-06 15:16:26 +0000

    each modules get mapped into the list which the user can choose from by clicking buttons

--- client/src/pages/modules.js
+++ client/src/pages/modules.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import { fetchUserModules } from "../api/lecturer.api";
 import Layout from "../components/layout";
 
@@ -18,7 +19,22 @@ const Modules = () => {
       <h1>Loading...</h1>
     </Layout>
   ) : (
-    <Layout>Modules Page</Layout>
+    <Layout>
+      <h2 className="text-center mt-5 mb-4">Your Modules</h2>
+      <div class="list-group">
+        {modules.map((module) => (
+          <NavLink to={`/modules`}>
+            <button
+              key={module.module_id}
+              type="button"
+              class="list-group-item list-group-item-action"
+            >
+              {module.module_name}
+            </button>
+          </NavLink>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
