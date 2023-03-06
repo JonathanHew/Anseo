b6dd51969d42587aa45c0cd26e5b7488ff0fdbcd
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-06 15:24:46 +0000

    created new page module report

---
+++ client/src/pages/moduleReport.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const ModuleReport = () => {
+  return (
+    <Layout>
+        Module Report Page 
+    </Layout>
+  )
+}
+
+export default ModuleReport
