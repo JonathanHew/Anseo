82d50f343ec94036cb8aaec52e14f24902cd343b
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-06 15:08:42 +0000

    useEffect to fetch users modules on render and store it in modules state

--- client/src/pages/modules.js
+++ client/src/pages/modules.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchUserModules } from "../api/lecturer.api";
 import Layout from "../components/layout";
 
 const Modules = () => {
+
+  const [modules, setModules] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const moduleContent = await fetchUserModules();
+      setModules(moduleContent.data.modules);
+      //setLoading(false);
+    })();
+  }, []);
   return <Layout>Modules Page</Layout>;
 };
 
