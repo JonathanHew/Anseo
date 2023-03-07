1d9f25429ab6303a2a099f79ff5a8c00fb5b7752
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-07 19:08:09 +0000

    created page selectSession which shows a table of users session so they can pick one

--- client/src/pages/selectSession.js
+++ client/src/pages/selectSession.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import Layout from "../components/layout";
 
 const SelectSession = () => {
-  return (
-    <div>SelectSession</div>
-  )
-}
+  const [sessions, setSessions] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default SelectSession
+  useEffect(() => {
+    (async () => {
+      const sessionContent = await fetchUserSessions();
+      setSessions(sessionContent.data.sessions);
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Layout>
+      <h4 className="text-center mt-5">Select a Session</h4>
+      <table className="table text-center">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>PIN</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Module</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sessions.map((session) => (
+            <tr key={session.session_id}>
+              <td>
+                <NavLink to={`/join/${session.session_id}`}>
+                  <span>{session.session_name}</span>
+                </NavLink>
+              </td>
+              <td>{session.session_pin}</td>
+              <td>{format(parseISO(session.session_date), "dd/MM/yyyy")}</td>
+              <td>{session.session_time}</td>
+              <td>{session.module_name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
+
+export default SelectSession;
