9883737a712934f9fa286c8ab804775d869e2947
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-08 17:04:17 +0000

    created useEffect to fetch students in session on render

--- client/src/pages/sessionReport.js
+++ client/src/pages/sessionReport.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchStudentsInSession } from "../api/lecturer.api";
 import Layout from "../components/layout";
 
 const SessionReport = () => {
-  return <Layout>
-    Session Report
-  </Layout>;
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { session_id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      await fetchStudentsInSession(session_id).then((res) => {
+        setStudents(res.data.students);
+      });
+
+      setLoading(false);
+    })();
+  }, []);
+  return loading ? (
+    <Layout>Loading ...</Layout>
+  ) : (
+    <Layout>Session Report</Layout>
+  );
 };
 
 export default SessionReport;
