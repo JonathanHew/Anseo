d7744198b143c1b43bde823529dbda04b0390e71
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-10 14:25:57 +0000

    created new delete signin in component, added a button

---
+++ client/src/components/deleteSignin.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const DeleteSignin = ({ signin_id }) => {
+  return (
+    <Fragment>
+      <button type="button" className="btn btn-danger">
+        X
+      </button>
+    </Fragment>
+  );
+};
+
+export default DeleteSignin;
