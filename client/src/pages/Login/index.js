e79e8996d8e2eee9049f6c507f7869558c89f753
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-12 17:05:30 +0000

    restyled login page

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { onLogin } from "../../api/auth";
 import Layout from "../../components/Layout";
-import {useDispatch} from "react-redux"
-import { authenticateUser } from "../../redux/slices/authSlice"; 
+import { useDispatch } from "react-redux";
+import { authenticateUser } from "../../redux/slices/authSlice";
+import "./style.css";
 
 const Login = () => {
   const [values, setValues] = useState({
@@ -20,6 +21,7 @@ const Login = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
+    setError();
 
     try {
       await onLogin(values);
@@ -33,47 +35,70 @@ const Login = () => {
 
   return (
     <Layout>
-      <form onSubmit={(e) => onSubmit(e)} className="container mt-3">
-        <h1>Login</h1>
+      <div id="login-form">
+        <h1 className="card-title text-center">Login</h1>
+        <p className="card-text text-center">
+          Login to enjoy all our cool features ✌️
+        </p>
+        <div className="card">
+          <div className="card-body">
+            <form onSubmit={(e) => onSubmit(e)} className="container mt-3">
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email address
+                </label>
+                <input
+                  onChange={(e) => onChange(e)}
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={values.email}
+                  placeholder="example@gmail.com"
+                  required
+                />
+              </div>
 
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input
-            onChange={(e) => onChange(e)}
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={values.email}
-            placeholder="example@gmail.com"
-            required
-          />
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  onChange={(e) => onChange(e)}
+                  type="password"
+                  value={values.password}
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  placeholder="password"
+                  required
+                />
+              </div>
+
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="flexCheck"
+                />
+                <label className="form-check-label" for="flexCheck">
+                  Remember me
+                </label>
+              </div>
+
+              <div style={{ color: "red", margin: "10px 0" }}>{error}</div>
+
+              <button
+                type="submit"
+                className="btn btn-primary mt-1 mb-3"
+                id="form-btn"
+              >
+                Login
+              </button>
+            </form>
+          </div>
         </div>
-
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            onChange={(e) => onChange(e)}
-            type="password"
-            value={values.password}
-            className="form-control"
-            id="password"
-            name="password"
-            placeholder="password"
-            required
-          />
-        </div>
-
-        <div style={{ color: "red", margin: "10px 0" }}>{error}</div>
-
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     </Layout>
   );
 };
