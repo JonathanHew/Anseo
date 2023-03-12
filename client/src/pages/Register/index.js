0f499d5bf3e14ad4ebb338cd084e5a3e9ea3ccdc
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-12 17:11:34 +0000

    Added styling to the Register Page

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { onRegistration } from "../../api/auth";
 import Layout from "../../components/Layout";
+import "./style.css"
 
 const Register = () => {
   const [values, setValues] = useState({
@@ -17,6 +18,8 @@ const Register = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
+    setError("");
+    setSuccess("");
 
     try {
       {
@@ -34,49 +37,61 @@ const Register = () => {
 
   return (
     <Layout>
-      <form onSubmit={(e) => onSubmit(e)} className="container mt-3">
-        <h1>Register</h1>
+      <div id="register-form">
+        <h1 className="card-title text-center">Register</h1>
+        <p className="card-text text-center">
+          Register to use all our cool features ✌️
+        </p>
+        <div className="card">
+          <div className="card-body">
+            <form onSubmit={(e) => onSubmit(e)} className="container mt-3">
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email address
+                </label>
+                <input
+                  onChange={(e) => onChange(e)}
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={values.email}
+                  placeholder="example@gmail.com"
+                  required
+                />
+              </div>
 
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input
-            onChange={(e) => onChange(e)}
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={values.email}
-            placeholder="example@gmail.com"
-            required
-          />
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  onChange={(e) => onChange(e)}
+                  type="password"
+                  value={values.password}
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  placeholder="password"
+                  required
+                />
+              </div>
+
+              <div style={{ color: "red", margin: "10px 0" }}>{error}</div>
+              <div style={{ color: "green", margin: "10px 0" }}>{success}</div>
+
+              <button
+                type="submit"
+                className="btn btn-primary mt-3 mb-3"
+                id="form-btn"
+              >
+                Register
+              </button>
+              <p className="text-muted text-center">Already a user? Login</p>
+            </form>
+          </div>
         </div>
-
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            onChange={(e) => onChange(e)}
-            type="password"
-            value={values.password}
-            className="form-control"
-            id="password"
-            name="password"
-            placeholder="password"
-            required
-          />
-        </div>
-
-        <div style={{ color: "red", margin: "10px 0" }}>{error}</div>
-        <div style={{ color: 'green', margin: '10px 0' }}>{success}</div>
-
-
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     </Layout>
   );
 };
