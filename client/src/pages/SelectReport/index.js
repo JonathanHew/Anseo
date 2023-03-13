b7cf0222e2c0c59a8a9c0aa183a0e7063108e957
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-03-13 17:28:39 +0000

    Created new page select report which will have 3 cards for each report and user selects one

---
+++ client/src/pages/SelectReport/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+const SelectReport = () => {
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row-cols-1 row-cols-md-3 g-4">
+          <div className="col-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Button
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Button
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Button
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default SelectReport;
