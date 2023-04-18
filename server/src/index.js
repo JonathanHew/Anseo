afc56311e9552d36200a5aabec72b5b885a66414
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-04-18 17:05:10 +0100

    added exposedHeaders to try solve set-cookies issue

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ require("./middlewares/passport-middleware");
 // initialize middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(cors({ origin: CLIENT_URL, credentials: true, exposedHeaders: ["set-cookie"]}));
 app.use(passport.initialize());
 
 // import routes
