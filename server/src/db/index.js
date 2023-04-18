5ebff9fcc8dae8cef21e44bdfc1d7ce589ac3a73
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-04-18 11:36:43 +0100

    DB connection information is now hidden in .env file

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,10 +1,18 @@
 const { Pool } = require("pg");
+const {
+  DB_USER,
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+} = require("../constants");
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "anseo_v2",
-  password: "postgres123",
-  port: 5432,
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
 });
 
 module.exports = {
